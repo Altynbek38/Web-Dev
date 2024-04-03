1a9cd16c598882c98452c4c3b042bc164601b4aa
Altynbek38 <altynbek4649@gmail.com>
2024-04-03 16:03:59 +0500

    meta class added

--- lab8/shop_back/api/models.py
+++ lab8/shop_back/api/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length = 500)
 
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
     def __str__(self):
         return self.name
     
@@ -20,6 +24,10 @@ class Product(models.Model):
     count = models.IntegerField()
     is_active = models.BooleanField()
     category = models.ForeignKey(Category, on_delete = models.CASCADE)
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
 
     def __str__(self):
         return self.name
